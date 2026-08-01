bbda171f842dc40a51ac9825f4b7eada68f5939a
Harshkale03 <harshalkale333@gmail.com>
2026-08-01 13:34:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-
+for(let i=0;i<35;i++){const p=document.createElement('div');p.className='particle';p.style.left=Math.random()*100+'vw';p.style.animationDelay=Math.random()*10+'s';document.body.appendChild(p);}
